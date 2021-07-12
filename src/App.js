706688b9dc7385011d8d924db6a42e7381c43b64
wangting <892184375@qq.com>
2021-07-12 15:10:46 +0800

    react和redux结合的demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+
+class App extends Component {
+  add1(){
+    this.props.onAdd1()
+  }
+  add2(){
+    this.props.onAdd2()
+  }
+  render(){
+    return (
+      <div className="App">
+        你点击了 <span id="value">{this.props.value}</span> 次
+        <div>
+          <button id="add1" onClick={this.add1.bind(this)}>+1</button>
+          <button id="add2" onClick={this.add2.bind(this)}>+2</button>
+          <button id="add1IfOdd" onClick={()=>{this.props.onAdd3()}}>如果是单数就+1</button>
+          <button id="add1After2Sec" onClick={()=>{this.props.onAdd4()}}>两秒钟后+1</button>
+        </div>
+      </div>
+    );
+
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,58 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { createStore } from 'redux';
+
+const reducer = (state, action)=>{
+  if(state === undefined){
+    return 0
+  }else{
+    if(action.type === 'add'){
+      var newState = state + action.payload
+      return newState
+    }else{
+      return state
+    }
+  }
+}
+
+const store = createStore(reducer) //初始化store
+render()
+store.subscribe(()=>{
+  render()
+})
+
+
+function add3(){
+  var oldState = store.getState()
+  if(oldState % 2 === 1){
+    store.dispatch({type:'add', payload: 1}) 
+  }
+}
+
+
+function add4(){
+  setTimeout(()=>{
+    store.dispatch({type: 'add', payload: 1})
+  },2000)
+}
+
+
+function render(){
+  ReactDOM.render(
+    <React.StrictMode>
+      <App value={store.getState()} 
+        onAdd1={()=>{store.dispatch({type: 'add', payload: 1})}}
+        onAdd2={()=>{store.dispatch({type: 'add', payload: 2})}}
+        onAdd3={add3}
+        onAdd4={add4}
+      />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
